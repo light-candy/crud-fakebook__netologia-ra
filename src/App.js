8f55008cecb6219f9774e28d1af479b1732b9f68
Svetlana Shamanova <schamanova.sv@gmail.com>
2020-10-20 18:04:11 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { PostsMainView } from './PostsMainView';
+import { NewPost } from './NewPost';
+import { EditPost } from './EditPost';
+import { PostWithButtons } from './Buttons';
+import { PostsProvider } from './PostsContext';
 import './App.css';
 
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+  return(
+    <PostsProvider>
+     <Router>
+       <Route path="/" exact component={PostsMainView} />
+       <Switch>
+         <Route path="/posts/new" exact component={NewPost} />
+         <Route path="/posts/:id" exact component={PostWithButtons} />
+       </Switch>
+       <Switch>
+         <Route path="/posts/:id/edit" exact component={EditPost} />
+       </Switch>
+     </Router>
+    </PostsProvider>
+  )
+};
 
 export default App;
